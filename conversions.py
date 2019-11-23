809000383603feb67fe8ae9f935f8fe3227befc1
Riddlerat <smcgahan@me.com>
2019-11-23 15:38:12 -0800

    add conversion.py

---
+++ conversions.py
@@ -0,0 +1,8 @@
+
+# Data source object for volume conversions 
+conversions = {
+    'tbsp': {'oz': 0.5000027, 'lt': 0.014787, 'tsp': 3.000008, 'tbsp': 1},
+    'tsp': {'oz': 0.5000027, 'lt': 0.014787, 'tsp': 1, 'tbsp': 0.3333324},
+    'oz': {'oz': 1, 'lt': 0.02957344, 'tsp': 5.999983, 'tbsp': 1.999989},
+    'lt': {'oz': 33.81413, 'lt': 1, 'tsp': 202.8842, 'tbsp': 67.62788},
+}
